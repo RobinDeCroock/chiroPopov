24abecf1aa78d5a507cf4f71cec2b577b7981bcd
robin.de.croock <decroock.robin@gmail.com>
2024-04-20 09:48:40 +0200

    Update chiro.scss and chiro.css with new styles for header-box and background colors, etc added js for navigation and templates for less copy past added images

---
+++ js/navigation.js
@@ -0,0 +1,26 @@
+function goToSection(sectionName) {
+    document.getElementById(sectionName).scrollIntoView();
+}
+
+function navigate(url)
+{
+    var baseUrl = window.location.origin;   
+    window.location  = baseUrl + url;
+}
+
+
+var flyerModal = document.getElementById('flyerModal')
+flyerModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  var source = button.getAttribute('data-bs-source')
+  var dataTitle = button.getAttribute('data-bs-title')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  var img = flyerModal.querySelector('.modal-body img')
+  var title = flyerModal.querySelector('.modal-title')
+  img.src = source
+  title.innerHTML = dataTitle
+})
