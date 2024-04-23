57422bb86b04af70081223ecd2db66c431b93194
robin.de.croock <decroock.robin@gmail.com>
2024-04-23 23:27:14 +0200

    Update navigation.js and nav.html with changes for active class in navigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,26 +1,32 @@
 function goToSection(sectionName) {
     document.getElementById(sectionName).scrollIntoView();
 }
-
 function navigate(url, id = null)
 {
     var baseUrl = window.location.origin;   
     window.location  = baseUrl + url;
+
+    if (id != null) {
+        // set class active
+        var element = document.getElementById(id);
+        var elements = document.getElementsByClassName('nav-link');
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].classList.remove('active');
+        }
+        element.classList.add('active');
+    }
 }
 
-
+if (document.getElementById('flyerModal')) {
 var flyerModal = document.getElementById('flyerModal')
 flyerModal.addEventListener('show.bs.modal', function (event) {
   // Button that triggered the modal
   var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
   var source = button.getAttribute('data-bs-source')
   var dataTitle = button.getAttribute('data-bs-title')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
   var img = flyerModal.querySelector('.modal-body img')
   var title = flyerModal.querySelector('.modal-title')
   img.src = source
   title.innerHTML = dataTitle
 })
+}
